9995ec9055ac8a326f95f6f7dc7868f5045d3469
sinarhen <nicklchannel8@gmail.com>
2022-07-07 12:53:30 +0300

    updated

--- 01_tdd_django/superlists/functional_tests.py
+++ 01_tdd_django/superlists/functional_tests.py
@@ -1,10 +1,30 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
 
-browser = webdriver.Chrome()
-browser.get('http://127.0.0.1:8000/')
+class NewVisitorTest(unittest.TestCase):
+    """Тест нового посетителя"""
+
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: можно начать список и получить его позже"""
+        # Эдит слышала про крутое новое онлайн приложение
+        # со списком неотложных дел
+        # она решает оценить его домашнюю страницу
+        self.browser.get('http://127.0.0.1:8000')
+
+        # Она видит что заголовок и шапка страницы говорят о списках
+        # неотложных дел
+        self.assertIn('The', self.browser.title)
+        self.fail('Закончить тест!')
 
 
-assert 'The' in browser.title
-print('completed successfully')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
