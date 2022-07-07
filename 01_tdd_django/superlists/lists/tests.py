aa10721d7bbae3349b6ab0db407ad83f48ff8a99
sinarhen <nicklchannel8@gmail.com>
2022-07-07 13:19:05 +0300

    Test works!

--- 01_tdd_django/superlists/lists/tests.py
+++ 01_tdd_django/superlists/lists/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
 
 # Create your tests here.
-class FirstTest(TestCase):
+class HomePageTest(TestCase):
+    """Test of home page"""
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 2)
+    def test_root_url_resolves_to_home_page_view(self):
+        """Test: the root url is converted to a view home page"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
